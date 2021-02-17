7d2204e058f7032fca6b562503f1df7c12bdf77d
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-17 17:58:03 +0000

    remove title tag, as this will be controlled by yoast

--- header.php
+++ header.php
@@ -10,11 +10,9 @@ $themeURL = get_template_directory_uri();
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="profile" href="http://gmpg.org/xfn/11">
-        <title><?php echo get_bloginfo( 'name' );?> - <?php echo get_bloginfo( 'description' ); ?></title>
 
         <?php wp_head(); ?>
 
     </head>
     
     <body <?php body_class(); ?>>
-        
