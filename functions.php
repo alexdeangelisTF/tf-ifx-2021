40fc653d6f84a5de14203184d618c0341d1ae75f
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-17 12:36:17 +0000

    link to latest bootstrap css

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ function your_scripts() {
 	/*Google Fonts*/
 	wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Roboto:300,400,700' );
 	/*Bootstrap 4*/
-	wp_enqueue_style( 'bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' );
+	wp_enqueue_style( 'bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css' );
 	/*Font Awesome*/
 	wp_enqueue_style( 'font-awesome', 'https://use.fontawesome.com/releases/v5.4.1/css/all.css' );
 	/*My Styles*/
