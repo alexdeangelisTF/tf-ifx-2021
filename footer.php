ed39f6b5e9c31f0f4c0ab8ca4353680672676e21
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-19 18:05:00 +0000

    footer - structure

--- footer.php
+++ footer.php
@@ -1,4 +1,41 @@
-<?php wp_footer(); ?>
+<?php
 
-</body>
-</html>
+echo '<footer>';
+echo '<div class="container">';
+echo '<div class="row">';
+echo '<div class="col-12 col-lg-4">';
+echo '</div>';
+echo '<div class="col-12 col-lg-8">';
+echo '<div class="row">';
+
+$menus = array(
+	'menu-footer-company',
+	'menu-footer-solutions',
+	'menu-footer-markets',
+	'menu-footer-legal',
+);
+
+foreach($menus as $menu) {
+	echo '<div class="col-6 col-lg-3">';
+	$menuName = wp_get_nav_menu_name($menu);
+	echo '<h5>';
+	echo $menuName;
+	echo '</h5>';
+	wp_nav_menu( 
+		array(
+			'theme_location' => $menu,
+		)
+	);
+	echo '</div>';
+}
+
+echo '</div>';
+echo '</div>';
+echo '</div>';
+echo '</div>';
+echo '</footer>';
+
+wp_footer();
+
+echo '</body>';
+echo '</html>';
