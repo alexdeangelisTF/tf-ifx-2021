ab9d6c95acf38bb4aafa03f7b72a0c3e6ce3b2f4
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-19 16:33:12 +0000

    header - hook menu right into the header
    Also, move both menus into a common div & link mobile collapse button to that div id

--- header.php
+++ header.php
@@ -21,26 +21,42 @@ $themeURL = get_template_directory_uri();
 			echo '<header>';
 			echo '<nav class="navbar navbar-expand-lg" role="navigation">';
 			echo '<div class="container">';
-			echo '<div class="row">';
-			echo '<div class="col-12">';
-			echo '<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false">';
+			echo '<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#ifx-navbar-collapse" aria-controls="bs-example-navbar-collapse" aria-expanded="false">';
 			echo '<span class="sr-only">Toggle navigation</span>';
 			echo '<span class="icon-bar top-bar"></span>';
 			echo '<span class="icon-bar middle-bar"></span>';
 			echo '<span class="icon-bar bottom-bar"></span>';
 			echo '</button>';
+			echo '<div class="row">';
+			echo '<div class="col-12 d-lg-flex justify-content-lg-between align-content-lg-center collapse navbar-collapse" id="ifx-navbar-collapse">';
+			echo '<div class="ifx-main-menu d-lg-flex">';
 			wp_nav_menu( 
 				array(
 					'theme_location' => 'main-menu',
 					'depth'           => 2,
 					'container'       => 'div',
-					'container_class' => 'collapse navbar-collapse',
+					'container_class' => '',
 					'container_id'    => 'bs-example-navbar-collapse-1',
 					'menu_class'      => 'nav navbar-nav',
 					'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
 					'walker'          => new WP_Bootstrap_Navwalker(),
 				)
 			);
+			echo '</div>';
+			echo '<div class="ifx-right-menu">';
+			wp_nav_menu( 
+				array(
+					'theme_location' => 'main-menu-right',
+					'depth'           => 1,
+					'container'       => 'div',
+					'container_class' => '',
+					'container_id'    => 'bs-example-navbar-collapse-2',
+					'menu_class'      => 'nav navbar-nav',
+					'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+					'walker'          => new WP_Bootstrap_Navwalker(),
+				)
+			);
+			echo '</div>';
 			echo '</div>';
 			echo '</div>';
 			echo '</div>';
