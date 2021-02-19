b3ff6d78c50b2f4a6734672e5e69fa0a3b0e9176
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-19 11:41:31 +0000

    basic header setup code
    wrapped in bootstrap container divs

--- header.php
+++ header.php
@@ -10,9 +10,27 @@ $themeURL = get_template_directory_uri();
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="profile" href="http://gmpg.org/xfn/11">
-
         <?php wp_head(); ?>
 
     </head>
     
     <body <?php body_class(); ?>>
+			
+			<?php
+			
+			echo '<header>';
+			echo '<div class="container">';
+			echo '<div class="row">';
+			echo '<div class="col-12">';
+			wp_nav_menu( 
+				array(
+					'theme_location' => 'main-menu'
+				)
+			);
+			echo '</div>';
+			echo '</div>';
+			echo '</div>';
+			echo '</header>';
+				
+				
+			?>
