86f728c0a094f19129e9f61f0acd24a04ad69834
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-22 17:46:40 +0000

    hero structure

--- includes/main/flexible.php
+++ includes/main/flexible.php
@@ -3,9 +3,10 @@
 if( have_rows('ifx_flexible_rows') ) {
 	while ( have_rows('ifx_flexible_rows') ) {
 		the_row();
+		$rowType = get_row_layout();
 		echo '<section>';
-		echo '<div class="ifx-row">';
-		if( get_row_layout() == 'hero' ) {
+		echo '<div class="ifx-row ifx-row-' . $rowType . '">';
+		if( $rowType == 'hero' ) {
 			include __DIR__ . '/rows/hero.php';
 		} else {}
 		echo '</div>';
--- includes/main/rows/hero.php
+++ includes/main/rows/hero.php
@@ -2,9 +2,32 @@
 
 $heading = get_sub_field('heading');
 $text = get_sub_field('text');
-$image = get_sub_field('image');
-if ($text) {
+$imageID = get_sub_field('image');
+
+echo '<div class="ifx-row-wrapper">';
+echo '<div class="container">';
+echo '<div class="row">';
+echo '<div class="col-12 col-md-6">';
+if ($heading) {
 	echo '<h1 class="weight-700">';
-	echo $text;
+	echo $heading;
 	echo '</h1>';
 }
+if ($text) {
+	echo '<h5>';
+	echo $text;
+	echo '</h5>';
+}
+echo '</div>';
+echo '<div class="col-12 col-md-6">';
+if ($imageID) {
+	$image = wp_get_attachment_image($imageID, 'full');
+	echo $image;
+}
+echo '</div>';
+echo '</div>';
+echo '</div>';
+echo '</div>';
+
+
+
