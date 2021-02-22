ac893e91e94d57bc6f4bda51672b0fb4ba0ad66d
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-22 19:51:27 +0000

    header - remove row class from dropdown hover js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 (function($) {
 	// Do this when hovering over desktop menu dropdown
-	$('header .row ul li.dropdown').mouseover(function(){
+	$('header ul li.dropdown').mouseover(function(){
 		$(this).addClass('show');
 		$(this).find('ul.dropdown-menu').addClass('show');
 	});
 	// Do this when hovering away from desktop menu dropdown
-	$('header .row ul li.dropdown').mouseout(function(){
+	$('header ul li.dropdown').mouseout(function(){
 		$(this).removeClass('show');
 		$(this).find('ul.dropdown-menu').removeClass('show');
 	});
