ebacbbc4a9aac181ff91e2031d4ff509f3665a3c
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-23 11:47:31 +0000

    hero tidy

--- includes/main/rows/hero.php
+++ includes/main/rows/hero.php
@@ -6,7 +6,15 @@ $imageID = get_sub_field('image');
 
 echo '<div class="ifx-row-wrapper">';
 echo '<div class="container">';
-echo '<div class="row">';
+echo '<div class="row flex-lg-row-reverse">';
+
+echo '<div class="col-12 col-md-6">';
+if ($imageID) {
+	$image = wp_get_attachment_image($imageID, 'full');
+	echo $image;
+}
+echo '</div>';
+
 echo '<div class="col-12 col-md-6">';
 if ($heading) {
 	echo '<h1 class="weight-700">';
@@ -19,15 +27,7 @@ if ($text) {
 	echo '</h5>';
 }
 echo '</div>';
-echo '<div class="col-12 col-md-6">';
-if ($imageID) {
-	$image = wp_get_attachment_image($imageID, 'full');
-	echo $image;
-}
-echo '</div>';
-echo '</div>';
-echo '</div>';
-echo '</div>';
 
-
-
+echo '</div>';
+echo '</div>';
+echo '</div>';
