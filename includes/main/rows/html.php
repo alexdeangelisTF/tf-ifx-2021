d4b4e3584438d7c2a6024a7a2878396862686fee
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-24 10:54:11 +0000

    row - html

---
+++ includes/main/rows/html.php
@@ -0,0 +1,17 @@
+<?php
+
+echo '<div class="ifx-row-wrapper">';
+
+echo '<div class="container">';
+echo '<div class="row">';
+echo '<div class="col-12">';
+
+echo '<div class="html__wrapper">';
+echo get_sub_field('html_code');
+echo '</div>';
+
+echo '</div>';
+echo '</div>';
+echo '</div>';
+
+echo '</div>';
