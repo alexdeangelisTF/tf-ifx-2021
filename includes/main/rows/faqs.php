498b42b924f4653a515671888176a8682d6fca05
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-24 17:30:14 +0000

    faqs html structure

--- includes/main/rows/faqs.php
+++ includes/main/rows/faqs.php
@@ -48,31 +48,33 @@ if ($questions) {
 		
 		if ($questionText && $answerText) {
 			
-			echo '<div class="card">';
+			
 			
 			if ($questionCount == 1) {
+				$cardOpen = 'open';
 				$headerLink = '<a data-toggle="collapse" data-parent="#accordionEx" href="#collapseOne' . $questionCount . '" aria-expanded="true" aria-controls="collapseOne' . $questionCount . '">';
-				$icon = '<i class="fas fa-angle-down rotate-icon"></i>';
 				$bodyDiv = '<div id="collapseOne' . $questionCount . '" class="collapse show" role="tabpanel" aria-labelledby="headingOne' . $questionCount . '" data-parent="#accordionEx">';
 			} else {
+				$cardOpen = false;
 				$headerLink = '<a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#collapseOne' . $questionCount . '"
         aria-expanded="false" aria-controls="collapseOne' . $questionCount . '">';
-				$icon = '<i class="fas fa-angle-down rotate-icon"></i>';
 				$bodyDiv = '<div id="collapseOne' . $questionCount . '" class="collapse" role="tabpanel" aria-labelledby="headingOne' . $questionCount . '"
       data-parent="#accordionEx">';
 			}
 			
+			echo '<div class="card ' . $cardOpen . '">';
+			
 			// Accordion Header
 			echo '<div class="card-header" role="tab" id="headingOne' . $questionCount . '">';
 			echo $headerLink;
-			echo '<h5 class="weight-700 d-flex justify-content-between align-items-center">' . $questionText . ' ' . $icon . '</h5>';
+			echo '<h5 class="weight-700 d-flex justify-content-between align-items-center">' . $questionText . ' <img src="' . get_template_directory_uri() . '/img/angle-down.svg" /></h5>';
 			echo '</a>';
 			echo '</div>';
 			
 			// Accordion Body
 			echo $bodyDiv;
 			echo '<div class="card-body">';
-			echo '<h6>' . $answerText . '</h6>';
+			echo '<h6 class="black-3">' . $answerText . '</h6>';
 			echo '</div>';
 			echo '</div>';
 			
