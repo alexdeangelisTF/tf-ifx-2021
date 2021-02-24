39920d9e5216608a69ff298fa438f1f016b00c9c
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-24 16:10:53 +0000

    row - tiles

--- includes/main/flexible.php
+++ includes/main/flexible.php
@@ -37,6 +37,8 @@ if( have_rows('ifx_flexible_rows') ) {
 			include __DIR__ . '/rows/logo-gallery.php';
 		} elseif ( $rowType == 'icon_text' ) {
 			include __DIR__ . '/rows/icon-text.php';
+		} elseif ( $rowType == 'tiles' ) {
+			include __DIR__ . '/rows/tiles.php';
 		} else {}
 		echo '</div>';
 		echo '</section>';
---
+++ includes/main/rows/tiles.php
@@ -0,0 +1,61 @@
+<?php
+$heading = false;
+$heading = get_sub_field('heading');
+$preheading = false;
+$preheading = get_sub_field('preheading');
+$tiles = false;
+$tiles = get_sub_field('tiles');
+
+echo '<div class="ifx-row-wrapper">';
+echo '<div class="container">';
+
+echo '<div class="tiles__wrapper">';
+
+echo '<div class="row">';
+echo '<div class="col-12">';
+echo '<div class="tiles__introduction">';
+if ($preheading) {
+	echo '<div class="caps">';
+	echo $preheading;
+	echo '</div>';
+}
+if ($heading) {
+	echo '<h2 class="weight-700">';
+	echo $heading;
+	echo '</h2>';
+}
+echo '</div>';
+echo '</div>';
+echo '</div>';
+
+if ($tiles) {
+	$tilesCount = count($tiles);
+	$colWidth = 12 / $tilesCount;
+	echo '<div class="row">';
+	foreach($tiles as $tile) {
+		echo '<div class="col-12 col-md-' . $colWidth . ' d-md-flex">';
+		echo '<div class="tile d-md-flex flex-md-column">';
+		echo '<div class="tile__images">';
+		if ($tile['image']) {
+			echo '<div class="tile__image">';
+			echo wp_get_attachment_image($tile['image'], 'medium');
+			echo '</div>';
+		}
+		echo '</div>';
+		echo '<div class="tile__content">';
+		if ($tile['heading']) {
+			echo '<h4 class="weight-700">' . $tile['heading'] . '</h4>';
+		}
+		if ($tile['text']) {
+			echo '<h6 class="black-3">' . $tile['text'] . '</h6>';
+		}
+		echo '</div>';
+		echo '</div>';
+		echo '</div>';
+	}
+	echo '</div>';
+}
+echo '</div>';
+
+echo '</div>';
+echo '</div>';
