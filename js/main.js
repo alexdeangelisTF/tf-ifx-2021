da69acaf84eb99e1b90249d751d433e69e9930f1
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-25 09:45:54 +0000

    faq - js for adding / removing class from card

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 (function($) {
+	// Header Dropdown
 	// Do this when hovering over desktop menu dropdown
 	$('header ul li.dropdown').mouseover(function(){
 		$(this).addClass('show');
@@ -9,4 +10,27 @@
 		$(this).removeClass('show');
 		$(this).find('ul.dropdown-menu').removeClass('show');
 	});
+	
+	// FAQs
+	$('.ifx-row-faqs .card .card-header a').on('click',function() {
+		
+		
+		// On click of card
+		var card = $(this).parent().parent();
+		// Check whether the clicked card has the open class
+		if ($(card).hasClass('open')) {
+			// If it does, remove the open class from the clicked card 
+			$(card).removeClass('open');
+		} else {
+			// If it doesn't, remove the open class from all cards in the faq
+			$('.ifx-row-faqs .card').each(function() {
+				$(this).removeClass('open');
+			})
+			
+			// And then add the open class to the clicked element
+			$(card).addClass('open');
+		}
+		
+	});
+	
 })( jQuery );
