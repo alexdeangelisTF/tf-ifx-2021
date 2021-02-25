d3b1468257f42ea0beabfd4f6d19458b7466ee53
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-25 14:36:00 +0000

    setting text-image row to use introduction css

--- includes/main/rows/image-text.php
+++ includes/main/rows/image-text.php
@@ -17,33 +17,22 @@ echo '<div class="ifx-row-wrapper">';
 
 echo '<div class="container">';
 echo '<div class="row ' . $rowReverse . '">';
-echo '<div class="col-12 col-md-7 d-flex align-items-center">';
+echo '<div class="col-12 col-md-6 col-lg-7 d-flex align-items-center">';
 echo '<div class="image-text__image">';
 echo  wp_get_attachment_image($imageID, 'full');
 echo '</div>';
 echo '</div>';
-echo '<div class="col-12 col-md-5 d-flex align-items-center">';
-echo '<div class="image-text__content">';
+echo '<div class="col-12 col-md-6 col-lg-5 d-flex align-items-center">';
+echo '<div class="image-text__introduction">';
 if ($preheader == 'text') {
-	echo '<div class="image-text__content__preheader">';
-	echo '<div class="image-text__content__preheader__' . $preheader . '">';
-	if ($preheader == 'text') {
-		$preheaderText = get_sub_field('preheader_text');
-		echo '<div class="caps">' . $preheaderText . '</div>';
-	} else {}
-	echo '</div>';
-	echo '</div>';
+	$preheaderText = get_sub_field('preheader_text');
+	echo '<div class="caps">' . $preheaderText . '</div>';
 }
-echo '<div class="image-text__content__heading">';
 echo '<h2 class="weight-700">' . $heading . '</h2>';
-echo '</div>';
-echo '<div class="image-text__content__text">';
 echo '<h5 class="black-3">' . $text . '</h5>';
-echo '</div>';
+
 if ($button) {
-	echo '<div class="image-text__content__button">';
 	echo '<a href="' . $button['url'] . '" class="button" target="' . $button['target'] . '">' . $button['title'] . '</a>';
-	echo '</div>';
 }
 echo '</div>';
 echo '</div>';
