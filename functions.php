2863ecd2a933431bdbb06edec0e42ea6378b8131
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-03-01 12:04:39 +0000

    glide js cdn link

--- functions.php
+++ functions.php
@@ -19,12 +19,18 @@ function your_scripts() {
 	wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css' );
 	/*Font Awesome*/
 	wp_enqueue_style( 'font-awesome', 'https://use.fontawesome.com/releases/v5.15.2/css/all.css' );
+	/*GlideJS CSS Core*/
+	wp_enqueue_style( 'glidejs-css-core', 'https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.4.1/css/glide.core.min.css' );
+	/*GlideJS CSS Theme*/
+	wp_enqueue_style( 'glidejs-css-theme', 'https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.4.1/css/glide.theme.min.css' );
 	/*Basic Styles*/
 	wp_enqueue_style( 'styles', get_stylesheet_uri(), array(), '1.0.0' );
 	/*IFX 2021 Styles*/
 	wp_enqueue_style( 'ifx-2021-styles', get_stylesheet_directory_uri() . '/css/ifx-2021.css', array(), '1.0.17' );
 	/*Bootstrap JS*/
 	wp_enqueue_script('bootstrap-js-bundle', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js', array('jquery'), null, true);
+	/*GlideJS*/
+	wp_enqueue_script('glidejs', 'https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.4.1/glide.min.js', array(), null, true);
 	/* IFX 2021 JS */
   wp_enqueue_script('ifx-2021-js', get_stylesheet_directory_uri() . '/js/main.js', array('jquery'), null, true);
 }
