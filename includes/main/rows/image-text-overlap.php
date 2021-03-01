4aec70fd009f929a85ff8b9f03d6013ffe48aeeb
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-03-01 11:46:06 +0000

    updating html structure

--- includes/main/rows/image-text-overlap.php
+++ includes/main/rows/image-text-overlap.php
@@ -12,11 +12,11 @@ $button = get_sub_field('button');
 $preheading = false;
 $preheading = get_sub_field('preheading');
 
-/*if ($layout == 'text_image') {
-	$rowReverse = 'flex-md-row-reverse';
+if ($layout == 'text_image') {
+	$rowReverse = 'rtl';
 } else {
 	$rowReverse = false;
-}*/
+}
 
 echo '<div class="ifx-row-wrapper">';
 
@@ -24,25 +24,74 @@ echo '<div class="image-text-overlap__wrapper">';
 
 echo '<div class="container">';
 echo '<div class="row">';
-echo '<div class="col-12 col-md-8">';
-echo '<div class="image-text__image" style="background-image:url(' . wp_get_attachment_image_url($imageID, 'full') . ')">';
-echo '</div>';
-echo '</div>';
+echo '<div class="col-12 col-image">';
 
-echo '<div class="col-12 col-md-7 introduction-wrapper">';
+echo '<div class="image-text-overlap__container ' . $rowReverse . '">';
+
+echo '<div class="one d-flex justify-content-end" style="background-image:url(' . wp_get_attachment_url($imageID) . ')">';
+
+echo '<div class="two d-none d-md-block">';
+
+echo '<div class="two-content">';
+
+/* Introduction */
 echo '<div class="image-text-overlap__introduction">';
 if ($preheading) {
-	echo '<div class="caps">' . $preheading . '</div>';
+	echo '<div class="caps">';
+	echo $preheading;
+	echo '</div>';
 }
-echo '<h2 class="weight-700">' . $heading . '</h2>';
-echo '<h5 class="black-3">' . $text . '</h5>';
+if ($heading) {
+	echo '<h2 class="weight-700">';
+	echo $heading;
+	echo '</h2>';
+}
+if ($text) {
+	echo '<h5 class="black-3">';
+	echo $text;
+	echo '</h5>';
+}
+
+if ($button) {
+	echo '<a href="' . $button['url'] . '" class="button" target="' . $button['target'] . '">' . $button['title'] . '</a>';
+}
+
+echo '</div>';
+
+
+echo '</div>';
+
+echo '</div>';
+
+echo '</div>';
+
+echo '</div>';
+
+echo '</div>';
+
+echo '<div class="col-12 d-md-none d-lg-none d-xl-none">';
+echo '<div class="two-content two-content-mobile">';
+/* Introduction */
+echo '<div class="image-text-overlap__introduction">';
+if ($preheading) {
+	echo '<div class="caps">';
+	echo $preheading;
+	echo '</div>';
+}
+if ($heading) {
+	echo '<h2 class="weight-700">';
+	echo $heading;
+	echo '</h2>';
+}
+if ($text) {
+	echo '<h5 class="black-3">';
+	echo $text;
+	echo '</h5>';
+}
 if ($button) {
 	echo '<a href="' . $button['url'] . '" class="button" target="' . $button['target'] . '">' . $button['title'] . '</a>';
 }
 echo '</div>';
-echo '</div>';
-
-echo '</div>';
 
 echo '</div>';
 echo '</div>';
@@ -50,7 +99,3 @@ echo '</div>';
 echo '</div>';
 
 echo '</div>';
-
-
-
-
