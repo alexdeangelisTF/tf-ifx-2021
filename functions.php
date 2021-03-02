1e14056de9de68d122ffd7a56a3556d63de65b82
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-03-02 17:17:13 +0000

    numeric pagination

--- functions.php
+++ functions.php
@@ -68,3 +68,18 @@ function register_navwalker(){
 	require_once get_template_directory() . '/includes/header/class-wp-bootstrap-navwalker.php';
 }
 add_action( 'after_setup_theme', 'register_navwalker' );
+
+// WP Query Numeric Pagination
+function wp_query_pagination( $custom_query ) {
+	$total_pages = $custom_query->max_num_pages;
+	$big = 999999999; // need an unlikely integer
+	if ($total_pages > 1){
+		$current_page = max(1, get_query_var('paged'));
+		echo paginate_links(array(
+			'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+			'format' => '?paged=%#%',
+			'current' => $current_page,
+			'total' => $total_pages,
+		));
+	}
+}
