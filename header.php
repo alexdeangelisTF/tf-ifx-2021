567b3861feb0960576cb6a504a4af821a5cc80c3
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-03-05 16:38:52 +0000

    updated css for reverse header

--- header.php
+++ header.php
@@ -1,14 +1,3 @@
-<?php
-// Get the name of the page template
-$pageTemplate = basename( get_page_template() );
-if ($pageTemplate = 'template-reverse-header.php') {
-	$template = 'reverse-header';
-} else {
-	$template = false;
-}
-
-?>
-
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js no-svg">
     <head>
@@ -23,7 +12,7 @@ if ($pageTemplate = 'template-reverse-header.php') {
 			
 			<?php
 			
-			echo '<header id="' . $template . '">';
+			echo '<header>';
 			echo '<nav class="navbar navbar-expand-lg" role="navigation">';
 			echo '<div class="container">';
 			echo '<div class="mobile-header d-lg-none">';
