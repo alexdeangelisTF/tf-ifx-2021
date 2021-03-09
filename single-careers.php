264db4aedc6263a9b53ceb5a24c384b73b93f181
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-03-09 14:41:48 +0000

    basic html

---
+++ single-careers.php
@@ -0,0 +1,18 @@
+<?php
+
+get_header();
+
+echo '<div id="main">';
+
+echo '<div class="default-background">';
+echo '</div>';
+
+echo '<div class="page-contents">';
+
+include __DIR__ . '/includes/main/templates/defaults/headline.php';
+
+echo '</div>';
+
+get_footer();
+
+?>
