0753cf05d3182942b13d275e5fb94347eca48afb
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-03-09 17:18:10 +0000

    update div class & include content template part

--- single-careers.php
+++ single-careers.php
@@ -7,9 +7,11 @@ echo '<div id="main">';
 echo '<div class="default-background">';
 echo '</div>';
 
-echo '<div class="page-contents">';
+echo '<div class="page-container">';
 
 include __DIR__ . '/includes/main/templates/defaults/headline.php';
+
+include __DIR__ . '/includes/main/templates/defaults/contents.php';
 
 echo '</div>';
 
