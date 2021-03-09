a039cf1babd6cc0afd889ac1bdbc70172892156d
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-03-09 17:17:12 +0000

    create apply page

---
+++ page-apply.php
@@ -0,0 +1,19 @@
+<?php
+
+get_header();
+
+echo '<div id="main">';
+
+echo '<div class="default-background">';
+echo '</div>';
+
+echo '<div class="page-container">';
+
+include __DIR__ . '/includes/main/templates/defaults/headline.php';
+
+
+echo '</div>';
+
+get_footer();
+
+?>
