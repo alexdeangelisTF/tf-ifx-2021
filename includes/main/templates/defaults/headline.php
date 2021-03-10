2d46a9c93784a706bf86ea698fdf8d0d2bfc600e
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-03-10 10:36:10 +0000

    enable custom fields from apply page

--- includes/main/templates/defaults/headline.php
+++ includes/main/templates/defaults/headline.php
@@ -4,12 +4,18 @@ $careerID = false;
 $careerName = false;
 $title = false;
 $pageClass = false;
-if (is_page('apply')) {
+$formID = false;
+$text = false;
+if (is_page_template('template-form.php')) {
 	$careerID = $_GET['id'];
 	$careerName = $_GET['job'];
-	$title = 'Application form';
-	$pageClass = 'page-apply';
+	$title = get_field('page_title');
+	$pageClass = 'template-apply';
 	$wrapperClass = 'col-12 col-md-10';
+} if (is_page_template('template-news.php')) {
+	$title = get_field('alternative_title');
+	$text = get_field('introduction_text');
+	$wrapperClass = 'col-12 col-md-7';
 } elseif (is_singular('careers')) {
 	$location = get_field('careers_location');
 	$title = get_the_title();
@@ -23,8 +29,6 @@ if (is_page('apply')) {
 	$wrapperClass = 'col-12 col-md-10';
 }
 
-
-
 echo '<div class="page-headline ' . $pageClass . '">';
 echo '<div class="container">';
 echo '<div class="row justify-content-md-center">';
@@ -32,7 +36,7 @@ echo '<div class="' . $wrapperClass . '">';
 if ($title) {
 	echo '<h1 class="weight-700">' . $title . '</h1>';
 }
-if (is_page('apply')) {
+if (is_page_template('template-form.php')) {
 	if ($careerID) {
 		$post = get_post( $careerID ); 
 		$jobName = $post->post_title;
@@ -43,6 +47,10 @@ if (is_page('apply')) {
 		echo '<div class="caps weight-700">' . $location . '</div>';
 	}
 	echo ifx_social_sharing_buttons();
+} elseif (is_page_template('template-news.php')) {
+	if ($text) {
+		echo '<h5 class="black-3">' . $text . '</h5>';
+	}
 } else {
 	echo ifx_social_sharing_buttons();
 }
