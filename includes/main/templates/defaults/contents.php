094f0fb2e2dc8f11798dc4da7058f1460c8eae08
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-03-10 10:37:05 +0000

    update HTML structure

--- includes/main/templates/defaults/contents.php
+++ includes/main/templates/defaults/contents.php
@@ -9,43 +9,58 @@ if (is_singular('careers')) {
 	$title = $post->post_name;
 }
 
+if (is_page_template('template-form.php')) {
+	$bodyWrapperClass = 'col-12';
+} else {
+	$bodyWrapperClass = 'col-12 col-md-10';
+}
+
 echo '<div class="page-contents">';
 
 echo '<div class="container">';
 echo '<div class="row justify-content-md-center">';
-echo '<div class="col-12 col-md-10">';
+echo '<div class="' . $bodyWrapperClass . '">';
 
 echo '<div class="page-contents__inner">';
 
-echo '<div class="page-contents__inner__text">';
 
-if (is_singular('careers')) {
-	if ($careersAboutIfxPayments) {
-		echo '<h4 class="weight-700">About IFX Payments</h4>';
-		echo $careersAboutIfxPayments;
-	}
-	if ($careersRole) {
-		echo '<h4 class="weight-700">Role</h4>';
-		echo $careersRole;
-	}
-	if ($careersResponsibilities) {
-		echo '<h4 class="weight-700">Responsibilities</h4>';
-		echo $careersResponsibilities;
-	}
-	if ($careersPackage) {
-		echo '<h4 class="weight-700">Package</h4>';
-		echo $careersPackage;
-	}
+if (is_page_template('template-form.php')) {
+	include __DIR__ . '/form.php';
 } else {
-	echo get_the_content();
-}
-echo '</div>';
+	echo '<div class="page-contents__inner__text">';
 
-if (is_singular('careers')) {
-	echo '<div class="button-wrap">';
-	echo '<a href="/apply?id=' . get_the_ID() . '&job=' . $title . '" class="button button-career">Apply now</a>';
+	if (is_singular('careers')) {
+		if ($careersAboutIfxPayments) {
+			echo '<h4 class="weight-700">About IFX Payments</h4>';
+			echo $careersAboutIfxPayments;
+		}
+		if ($careersRole) {
+			echo '<h4 class="weight-700">Role</h4>';
+			echo $careersRole;
+		}
+		if ($careersResponsibilities) {
+			echo '<h4 class="weight-700">Responsibilities</h4>';
+			echo $careersResponsibilities;
+		}
+		if ($careersPackage) {
+			echo '<h4 class="weight-700">Package</h4>';
+			echo $careersPackage;
+		}
+	} else {
+		echo get_the_content();
+	}
 	echo '</div>';
+
+	if (is_singular('careers')) {
+		echo '<div class="button-wrap">';
+		echo '<a href="/apply?id=' . get_the_ID() . '&job=' . $title . '" class="button button-career">Apply now</a>';
+		echo '</div>';
+	}
 }
+
+
+
+
 
 echo '</div>';
 
