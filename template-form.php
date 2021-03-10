b05fccbb425c5b166ab73c2a111eb00c6071ad3f
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-03-10 10:35:51 +0000

    create form template

---
+++ template-form.php
@@ -0,0 +1,21 @@
+<?php
+
+/* Template Name: Form */
+
+get_header();
+
+echo '<div id="main">';
+
+echo '<div class="default-background">';
+echo '</div>';
+
+echo '<div class="page-container template-form">';
+
+include __DIR__ . '/includes/main/templates/defaults/headline.php';
+include __DIR__ . '/includes/main/templates/defaults/contents.php';
+
+echo '</div>';
+
+get_footer();
+
+?>
