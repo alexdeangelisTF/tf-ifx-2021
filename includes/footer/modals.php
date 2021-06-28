eb5b3c8df07ccab8a1d639ec6ae7e7c962c99ed4
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-06-28 12:14:29 +0100

    include modal ability for hero row

--- includes/footer/modals.php
+++ includes/footer/modals.php
@@ -1,12 +1,12 @@
 <?php
 
-//$rowCount = 1;
+$rowCount = 1;
 if( have_rows('ifx_flexible_rows') ) {
 	while ( have_rows('ifx_flexible_rows') ) {
 		the_row();
 		$rowType = get_row_layout();
 		
-		if ( $rowType == 'hero_video_background' ) {
+		if ( $rowType == 'hero_video_background' || $rowType == 'hero') {
 			$videoModal = false;
 			$videoModal = get_sub_field('video_modal');
 			
@@ -25,30 +25,41 @@ if( have_rows('ifx_flexible_rows') ) {
 					$iFrame = '<iframe src="' . $videoUrl . '" frameborder="0" allowfullscreen></iframe>';
 				} else {}
 				
-				// Hero Video Background Modal Settings
-				echo '<!-- Modal -->
-				<div class="modal modal__video-background fade" id="videoBGModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-					<div class="modal-dialog modal-dialog-centered">
-						<div class="modal-content">
-							<div class="modal-header">
-								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-									<span aria-hidden="true"><i class="fas fa-times"></i></span>
-								</button>
-							</div>
-							<div class="modal-body source_' . $videoSource . '">
-								<div class="embed-container">
-									' . $iFrame . '
+				if ( $rowType == 'hero_video_background') {
+					$modalID = 'videoBGModal_' . $rowCount;
+				} elseif ($rowType == 'hero') {
+					$modalID = 'heroModal_' . $rowCount;
+				} else {
+					$modalID = false;
+				}
+
+				if ($modalID) {
+
+					// Hero Video Background Modal Settings
+					echo '<!-- Modal -->
+					<div class="modal modal__video-background fade" id="' . $modalID . '" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+						<div class="modal-dialog modal-dialog-centered">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true"><i class="fas fa-times"></i></span>
+									</button>
+								</div>
+								<div class="modal-body source_' . $videoSource . '">
+									<div class="embed-container">
+										' . $iFrame . '
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				</div>';
+					</div>';
+
+				}
 				
 			}
 			
-			
 		} else {}
-		//$rowCount++;
+		$rowCount++;
 	}
 }
 
